0b9476095c9bde6a8771ed36fa38aeff215d4e84
TatsuyaAbe <45484598+AT410@users.noreply.github.com>
2020-07-07 09:35:32 +0900

    [new][map]マップエディタプロジェクト追加

---
+++ 2DMapEditor/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2DMapEditor
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool Dirty = false;
+        private string FileName = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.FileName = "";
+            dialog.DefaultExt = "*.*";
+            if (dialog.ShowDialog() == true)
+            {
+
+            }
+        }
+
+        private void SaveFile_Click(object sender,RoutedEventArgs e)
+        {
+            if(FileName == "")
+            {
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.FileName = "MapFile";
+                dialog.Filter = "xml files (*.xml)|*.xml|All files (*.*)|*.*"; ;
+                if(dialog.ShowDialog() == true)
+                {
+                    string FileName = dialog.FileName;
+                    SaveFiles(FileName);
+                }
+            }
+            else
+            {
+                SaveFiles(FileName);
+            }
+        }
+
+        private void SaveFiles(string FullPath)
+        {
+            if (!Dirty)//変更がなければ戻る
+                return;
+
+            //ファイルを保存する
+        }
+    }
+}
